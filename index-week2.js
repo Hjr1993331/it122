6644a585cc632b879d5ea9d43c968b7d4e4312f2
Hijiri <hjroath1993@gmail.com>
2021-07-18 16:24:17 -0700

    Assignment 2 commit added /detail

---
+++ index-week2.js
@@ -0,0 +1,35 @@
+console.log('Hello')
+import { getAll, getItem } from './data.js';
+import http from 'http';
+import { parse } from "querystring";
+// console.log(getItem("Avengers"))
+
+http.createServer (
+    (req, res) => {
+        
+    let url = req.url.split("?");  
+    let query = parse(url[1]); 
+    var path = url[0].toLowerCase();
+    console.log(query)
+
+        if (path === '/') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(JSON.stringify(getAll()));
+        }
+        
+        if (path === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(getItem("Avengers")));
+        }
+
+        if (path === '/detail') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(JSON.stringify(getItem('Avengers')));
+            }
+        
+        else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+        }
+        
+}).listen(process.env.PORT || 3000);
